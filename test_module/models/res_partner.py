425412f32dba0bfbcdd2848551f42a9f0ffce3c3
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2018-07-26 10:08:28 +0530

    [TEST] Testing

--- test_module/models/res_partner.py
+++ test_module/models/res_partner.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
